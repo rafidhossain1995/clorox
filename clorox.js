2ed6035b1cf962141d068c227b7ffca44838eecc
rafidhossain1995 <rafidhossain@pursuit.org>
2021-02-01 08:56:33 -0500

    test and final code submission

--- clorox.js
+++ clorox.js
@@ -2,4 +2,19 @@
 // For multiples of 3, print “CLX” instead of the number and for the multiples of 5, print “DTC”. 
 // For numbers that are a multiple of 3 and 5, it should print “CLXDTC”.
 
- 
+ const CLXDTC = ()=>{
+     for(let i = 1; i <= 100; i++){
+         if(i%3 === 0 && i%5 === 0 ){
+             console.log("CLXDTC")
+         }else if(i%3 === 0){
+             console.log("CLX")
+         }else if (i%5 === 0){
+             console.log("DTC")
+         }else{
+             console.log(i)
+         }
+     }
+ }
+console.log(CLXDTC())
+
+
