bdf6d92d4b92d47bb211da719fd0a54b1732ac70
rafidhossain1995 <rafidhossain@pursuit.org>
2021-01-31 20:08:43 -0500

    add question

---
+++ clorox.js
@@ -0,0 +1,5 @@
+// The program should print the numbers 1 to 100. 
+// For multiples of 3, print “CLX” instead of the number and for the multiples of 5, print “DTC”. 
+// For numbers that are a multiple of 3 and 5, it should print “CLXDTC”.
+
+ 
